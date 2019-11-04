f9c636f82cdca5b327b66b295fc2d143b2849600
johnmogi <anguru@gmail.com>
2019-11-04 13:15:31 -0500

    solved 1

---
+++ 1/js/main.js
@@ -0,0 +1,21 @@
+/// <reference path="jquery-3.4.1.js" />>
+$( document ).ready(function() {  
+// $("card").each.html(this + 1)
+ 
+
+//   let index = 1;
+
+// $(".card").click(function() {
+
+//     $(".card-title").each(function() {
+// alert
+//     });
+//   });
+
+$(".card").click(function() {
+    alert($(this).text());
+
+});
+
+});
+
