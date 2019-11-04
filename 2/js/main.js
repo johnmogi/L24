242e35cb26e57020d2a9ee98c611da435fe03c1a
johnmogi <anguru@gmail.com>
2019-11-04 13:26:57 -0500

    the same excercises omg

---
+++ 2/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+(function(){
+    setInterval(() => {
+        const main = document.getElementById("main");
+        main.innerHTML = "";
+        main.append(Math.floor(Math.random() * 101));
+      }, 1000);
+})();
