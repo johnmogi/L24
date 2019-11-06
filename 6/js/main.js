89eda887cbddaaa939f1dfb3f238bd19d43dcdec
johnmogi <anguru@gmail.com>
2019-11-06 01:56:49 -0500

    improve

---
+++ 6/js/main.js
@@ -0,0 +1,14 @@
+
+
+"use strict";
+
+(function(){
+
+    function nice(colorGreen) {
+        colorGreen('Green');
+    }
+
+  nice(()=>document.body.bgColor='green')
+
+//TODO  ask for example on this in class
+})();
